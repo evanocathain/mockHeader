da19248d4f0b2d4ddf93297c2629aedde92b9ea3
David <dmckenna@cp.dias.ie>
2020-11-17 14:48:53 +0000

    Fix being unable to specify a negative declination

--- mockHeader.c
+++ mockHeader.c
@@ -131,7 +131,7 @@ int main(int argc, char *argv[]) {
             strcpy(DecTxt, argv[argument+1]);
             sscanf(DecTxt, "%lf:%lf:%lf", &DecD, &DecM, &DecS);
             if (strncmp (DecTxt,"-",1) == 0) {
-               src_dej = (DecD * -10000.0) + (DecM * -100.0) + (-1.0 * DecS);
+               src_dej = (DecD * 10000.0) + (DecM * -100.0) + (-1.0 * DecS);
             } else {
                src_dej = (DecD * 10000.0) + (DecM * 100.0) + ( DecS * 1.0);
             }
